45265309789b159118945aa2bb5293c9526879a5
Victor Sunny <doobam007@gmail.com>
2025-08-17 15:11:13 +0100

    improved accessibility by adding keyboard triggers that close popup modals and action dialogues

--- src/components/Backdrop/Backdrop.jsx
+++ src/components/Backdrop/Backdrop.jsx
@@ -5,12 +5,22 @@ import './Backdrop.css';
 function Backdrop({ setPopoverDisplayState }) {
   //// ADD BACKROP UPON DISPLAY OF POPOVER ELEMENT
 
+  // function for closing modal on special keypresses for improved accessibility
+  const handleKeyClick = (e) => {
+    ((e.key == 'Escape') || (e.key == 'Enter') || (e.key == 'Backspace')) 
+    && setPopoverDisplayState(false);
+  }
+  
   useEffect(() => {
+    // add event listener for escape
+    document.addEventListener('keydown', handleKeyClick)
     // make the page unscrollable
     document.documentElement.classList.toggle('no-scroll');
     // UNMOUNTING - make page scrollable
     return () => {
+      // reset modifications to page
       document.documentElement.classList.remove('no-scroll');
+      document.removeEventListener('keydown', handleKeyClick)
     };
   }, []);
 
